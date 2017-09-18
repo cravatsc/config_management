af06388abb4c5afc966ca439555324fa0235e831
Craig Cravath <craigcravath@gmail.com>
2017-09-17 22:36:36 -0400

    modifying package setup

---
+++ config_management/config_manager.py
@@ -0,0 +1,11 @@
+class ConfigManager:
+    config = {}
+
+    def __init__(self, file):
+        self.read_file_to_dict(file)
+
+    def read_file_to_dict(self, infile):
+        with open(infile) as conf:
+            for line in conf:
+                (key, value) = line.split('=')
+                self.config[key] = value
