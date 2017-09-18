2b7b131b41cb8c098e1adb049aba16eb845aac93
Craig Cravath <craigcravath@gmail.com>
2017-09-17 22:29:51 -0400

    add set up to package modules

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
         name='config_management',
-        version='0.1'
+        version='0.1',
         url='https://github.com/cravatsc/config_management.git',
         packages=['config_management'])
